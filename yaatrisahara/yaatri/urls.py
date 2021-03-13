d8685ce0759fba6419d9299f8b8966ec6107fea2
hrichamaharjan <richoo234@gmail.com>
2021-03-13 15:42:34 +0545

    blog-updated

--- yaatrisahara/yaatri/urls.py
+++ yaatrisahara/yaatri/urls.py
@@ -1,8 +1,14 @@
 
 from django.urls import path
+from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView)
 from . import views
 
 urlpatterns = [
     path('',views.home),
-    path('blog', views.blog,name='blog')
+    path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('user/<str:username>', UserPostListView.as_view(),name='user-posts'),
+    path('blog', PostListView.as_view(),name='blog')
 ]
